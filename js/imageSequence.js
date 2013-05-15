f3bd17c426828c2081bf3429af0f1295c1261c78
Ryan Berkey <iamryanberkey@gmail.com>
2013-05-15 16:26:56 -0400

    duh

--- js/imageSequence.js
+++ js/imageSequence.js
@@ -6,8 +6,9 @@ scenes: [
 			// Example set piece:
 			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
 			{type: 'image', source: 'images/phantom/fire_00000.png', coords: [  0,   0, 0]},
-			{type: 'image', source: 'images/phantom/fire_00000.png', coords: [  0,   0, 0]},
-			{type: 'image', source: 'images/TWD_plxD-slum03.png', coords: [  0,   0, 0]}
+			{type: 'image', source: 'images/phantom/fire_00001.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/phantom/fire_00002.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/phantom/fire_00002.png', coords: [  0,   0, 0]},
 		]},
 		{coords: [1200,60], pieces: [
 			// Example set piece:
