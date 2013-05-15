da9dbfedba5a51b935361436f7723fe250e9643e
Ryan Berkey <iamryanberkey@gmail.com>
2013-05-15 16:23:16 -0400

    imageSequence

---
+++ js/imageSequence.js
@@ -0,0 +1,28 @@
+var imageSequence = {
+scenes: [
+		// Example Scene:
+		//     {coords: [r, theta], pieces: [ ... ]},
+		{coords: [1200,0], pieces: [
+			// Example set piece:
+			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
+			{type: 'image', source: 'images/phantom/fire_00000.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/phantom/fire_00000.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/TWD_plxD-slum03.png', coords: [  0,   0, 0]}
+		]},
+		{coords: [1200,60], pieces: [
+			// Example set piece:
+			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
+			{type: 'image', source: 'images/TWD_plxD-slum01.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/TWD_plxD-slum02.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/TWD_plxD-slum03.png', coords: [  0,   0, 0]}
+		]},
+		{coords: [1200,60], pieces: [
+			// Example set piece:
+			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
+			{type: 'image', source: 'images/TWD_plxD-slum01.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/TWD_plxD-slum02.png', coords: [  0,   0, 0]},
+			{type: 'image', source: 'images/TWD_plxD-slum03.png', coords: [  0,   0, 0]}
+		]},
+	]
+
+}
