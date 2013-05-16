57e94f1955735aa7cf0ae4e2431eef6aff37cd0b
Ryan Berkey <iamryanberkey@gmail.com>
2013-05-16 12:26:00 -0400

    CSS Fixes

--- js/resourceLibrary.js
+++ js/resourceLibrary.js
@@ -1,6 +1,6 @@
 /* The Resources Library contains an ordered list of chapters.
  */
-var hexSize = 2000;
+var hexSize = 2500;
 var resourceLibrary = {
 	tourDuration: 1000, // Length of documentary, in seconds.
 	scenes: [
@@ -14,16 +14,16 @@ var resourceLibrary = {
 		{coords: [hexSize,60], time: 40, year: '1950', title: 'Tent Camp', pieces: [
 			// Example set piece:
 			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
-			{type: 'date', text: ['1950'], coords: [  -500, -375, -10]},
+			{type: 'date', text: ['1950'], coords: [  -1000, -350, -20]},
 			{type: 'text', text: ['India\'s magicians, acrobats, and puppeteers leave the rural villages for New Delhi.', 
-			'They set up a tent camp called \"The Kathputli Colony.\"'], coords: [  100,   0, -200]},
+			'They set up a tent camp called \"The Kathputli Colony.\"'], coords: [  500,   0, -200]},
 			{type: 'image', source: 'images/scene1/compressed/tents4.png', coords: [  0,  -100,  10], dimentions: [2000, 548]},
 			{type: 'image', source: 'images/scene1/compressed/tents3.png', coords: [  650,   -100,  750], dimentions: [1974, 512]},
 			{type: 'image', source: 'images/scene1/compressed/tents2.png', coords: [  450,   -75,  1000], dimentions: [1974, 758]},
 		]},
 		{coords: [hexSize,120], time: 50, year: '1964', title: 'Artists Village', pieces: [
 			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
-			{type: 'date', text: ['1964'], coords: [  -500, -250, -10]},
+			{type: 'date', text: ['1950'], coords: [  -1000, -350, -20]},
 			{type: 'text', text: ['They petition the government to turn the Kathputli Colony into an artists village.\n\
 			The call their dream village, Anangdram: \"The Joy Village.\"'], coords: [  0,   0, -200]},
 			{type: 'image', source: 'images/scene2/compressed/buildings.png', coords: [  -165,   -325, 1], dimentions: [800*1.35, 89*1.35]},
@@ -35,7 +35,7 @@ var resourceLibrary = {
 		{coords: [hexSize,180], time: 100, year: '2011', title: 'Slum', pieces: [
 			// Example set piece:
 			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
-			{type: 'date', text: ['2011'], coords: [  -500, -250, -10]},
+			{type: 'date', text: ['1950'], coords: [  -1000, -350, -20]},
 			{type: 'text', text: ['The government never gave the artists the rights to the land.\n\
 			It has become a slum.'], coords: [  0,   0, -200]},
 			{type: 'image', source: 'images/compressed/TWD_plxD-slum01.png', coords: [  0,   0, -300]},
@@ -46,6 +46,7 @@ var resourceLibrary = {
 		{coords: [hexSize,240], time: 270, year: '2011', title: 'Slum', pieces: [
 			// Example set piece:
 			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
+			{type: 'date', text: ['1950'], coords: [  -1000, -350, -20]},
 			{type: 'image', source: 'images/scene3/compressed/puran.png', coords: [  0,   0, -300]},
 			{type: 'image', source: 'images/scene3/compressed/headstandboy.png', coords: [  0,   0, -100]},
 			{type: 'image', source: 'images/scene3/compressed/wallcart.png', coords: [  0,   0, -100]},
@@ -56,7 +57,7 @@ var resourceLibrary = {
 		{coords: [hexSize,300], time: 340, year: '2014', title: 'Skyscraper', pieces: [
 			// Example set piece:
 			//     {type: 'image or video', source: 'a uri, or {webm: 'a uri', etc}', coords: [x, y, z], dimentions: [width, height]}
-			{type: 'date', text: ['2014'], coords: [  -500, -250, -10]},
+			{type: 'date', text: ['1950'], coords: [  -1000, -350, -20]},
 			{type: 'text', text: ['The government announces the artists\' land has been sold to a real-estate developer.\n\
 			They plan to build New Delhi\'s first skyscraper on the Kathputli land.\n\
 			2,800 artist families will move into transit camps.'], coords: [  0,   0, -200]},
